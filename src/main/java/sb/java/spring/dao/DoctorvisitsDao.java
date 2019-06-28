4c8b0ee22191184f4a5e1d2af890234fc5eecf20
lsb <lsb@DESKTOP-JGG1RV2>
2019-06-28 18:07:08 +0800

    4

--- src/main/java/sb/java/spring/dao/DoctorvisitsDao.java
+++ src/main/java/sb/java/spring/dao/DoctorvisitsDao.java
@@ -7,7 +7,7 @@ import sb.java.spring.entity.Doctorvisits;
 public interface DoctorvisitsDao {
 
 	//添加医生问诊病历
-	@Insert("insert into doctorvisits(username,title,describes,status) values (#{username},#{title},#{describes},#{status})")
+	@Insert("insert into doctorvisits(username,title,describes,status) values (#{username},#{title},#{describes},#{username})")
 	Doctorvisits addDov(Doctorvisits doctorvisits);
 
 }
